370bedfce201b746c8197c59da052c1d91d9abcb
Brunna Carvalho <114610031+brunnarayssa@users.noreply.github.com>
2025-02-21 18:10:15 +0000

    subindo proyecto

--- src/js/views/demo.js
+++ src/js/views/demo.js
@@ -7,37 +7,87 @@ import "../../styles/demo.css";
 
 export const Demo = () => {
 	const { store, actions } = useContext(Context);
+	const navigate = useNavigate();
+
+	
+	const [showModal, setShowModal] = useState(false);
+	const [contactToDelete, setContactToDelete] = useState(null);
+
+	useEffect(() => {
+		actions.obtenerContacts(); // Cargar contactos al montar el componente
+	}, []);
+
+	const handleClose = () => setShowModal(false);
+	const handleShow = (contactId) => {
+		setContactToDelete(contactId);
+		setShowModal(true);
+	};
+
+	const confirmDelete = () => {
+		actions.deleteContact(contactToDelete);
+		setShowModal(false);
+	}
 
 	return (
 		<div className="container">
-			<ul className="list-group">
-				{store.demo.map((item, index) => {
-					return (
-						<li
-							key={index}
-							className="list-group-item d-flex justify-content-between"
-							style={{ background: item.background }}>
-							<Link to={"/single/" + index}>
-								<span>Link to: {item.title}</span>
-							</Link>
-							{// Conditional render example
-							// Check to see if the background is orange, if so, display the message
-							item.background === "orange" ? (
-								<p style={{ color: item.initial }}>
-									Check store/flux.js scroll to the actions to see the code
+			<nav className="navbar  mb-3">
+				<div className="d-flex justify-content-end w-100">
+					<Link to="/add">
+						<button className="btn btn-success">Add new contact</button>
+					</Link>
+				</div>
+			</nav>
+
+		<ul className="list-group">
+			{/*si es un array y tiene elementos se muestra los contactos usando map, si esta vacio o no es un array mostra msg de no hay contactos */}
+			{Array.isArray(store.contacts) && store.contacts.length > 0 ? (
+			store.contacts.map((item, index) => {
+				return (
+					<li key={index} className="list-group-item d-flex justify-content-between ">
+						<div className="d-flex align-items-center">
+							<img src="https://picsum.photos/id/64/150/150" 	alt="Contact" className="img-circle"/>
+							<div className="datos-del-contacto ml-3">
+								<h3>{item.name}</h3> 
+								<p>
+									<FontAwesomeIcon icon={faLocationDot} style={{color: "#646464", marginRight: "10px"}} />
+									{item.address}
+									<br/>
+									<FontAwesomeIcon icon={faPhone} flip="horizontal" style={{color: "#646464", marginRight: "10px"}} />
+									{item.phone}
+									<br/>
+									<FontAwesomeIcon icon={faEnvelope} style={{color: "#646464", marginRight: "10px"}} />
+									{item.email}
 								</p>
-							) : null}
-							<button className="btn btn-success" onClick={() => actions.changeColor(index, "orange")}>
-								Change Color
-							</button>
-						</li>
-					);
-				})}
-			</ul>
-			<br />
-			<Link to="/">
-				<button className="btn btn-primary">Back home</button>
-			</Link>
+							</div>
+						</div>
+						<div className="iconos">
+						<FontAwesomeIcon icon={faPen} onClick={()=> navigate(`/edit/${item.id}`)} className="m-3"/>{/*para navegar a la pagina de edicion del contacto */}
+						<FontAwesomeIcon icon={faTrashCan} onClick={()=>handleShow(item.id)} className="m-3"/>
+						</div>
+					</li>
+				);
+			})
+			) : (<p>No contacts found.</p> // Muestra un mensaje si no hay contactos
+			)}
+		</ul>
+			{/*operador ternario si showModal es true abre el modal si es falso oculta el modal */}
+		<div className={`modal ${showModal ? 'd-block' : 'd-none'}`} tabindex="-1">
+			<div className="modal-dialog">
+				<div className="modal-content">
+					<div className="modal-header">
+						<h5 className="modal-title">Are you sure?</h5>
+						<button type="button" className="btn-close" onClick={handleClose} aria-label="Close"></button>
+					</div>
+					<div className="modal-body">
+						<p>If you delete this thing the entire universe will go down!</p>
+					</div>
+					<div className="modal-footer">
+						<button type="button" className="btn btn-primary" onClick={handleClose}>Oh no!</button>
+						<button type="button" className="btn btn-secondary" onClick={confirmDelete}>Yes baby!</button>
+					</div>
+				</div>
+			</div>
 		</div>
-	);
+	</div>
+);
 };
